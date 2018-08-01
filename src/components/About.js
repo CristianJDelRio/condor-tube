e6ac1d1a2cfab001680b950a14753d4658b53e91
Cristian Del Río Martinez <cristianjdelriom@gmail.com>
2018-08-01 16:34:43 -0500

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,18 @@ class About extends Component {
   render() {
     return (
         <div className='about'>
-        <h1>About Me</h1>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+        <h1>Author:</h1>
+        <h2>Cristian Del Río Martinez</h2>
+        <h3>Web Developer</h3>
+        <h3>Cel. 3004323978</h3>
+        <h3>@CristianJDelRio</h3>
+        <br/>
+        <br/>
+        <h4>Check Code: </h4>
+        
+        <a href='https://github.com/CristianJDelRio/condor-tube' className='gitHub Logo'>
+        <i className="fab fa-github"></i>
+        </a>
       </div>
     );
   }
